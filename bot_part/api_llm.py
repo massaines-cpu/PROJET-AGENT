9a18eece6eaf46a8d255c27a72735ae9f7e4e8cf
inesmassa <inesurvivor@gmail.com>
2026-04-14 14:00:43 +0200

    prompt api

--- bot_part/api_llm.py
+++ bot_part/api_llm.py
@@ -12,6 +12,8 @@ class Demande(BaseModel):
     demande_pour_repondre: str
 
 #endpoint doit recevoir une demande str et retourner la réponse du LLM.
+with open('./agent_part/swagger.json', 'r') as f:
+    contenu = f.read()
 
 @app.post('/demande')
 def demande_llm(request: Demande):
@@ -19,6 +21,11 @@ def demande_llm(request: Demande):
     reponse = gpt.chat.completions.create(
         model='gpt-5.1-chat',
         messages=[
-            {'role': 'system', 'content': 'tu es un agent, réponds uniquement en français'},
-            {'role': 'user', 'content': demande }])
+            {'role': 'system', 'content': f'''Tu es un agent. À partir de la demande utilisateur, 
+            réponds UNIQUEMENT en JSON avec ce format :
+            
+            {{"liste_des_actions": [{{"action": "nom_action", "parametres": ["param1"]}}]}}
+            Voici les actions disponibles : {contenu}'''},
+
+            {'role': 'user', 'content': demande}])
     return reponse.choices[0].message.content
